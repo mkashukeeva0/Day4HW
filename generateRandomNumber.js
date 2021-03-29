b7435cefa8a67666426cba1918533b58eb78713d
Mariela Kashukeeva <72321310+mkashukeeva0@users.noreply.github.com>
2021-03-29 13:35:56 +0100

    Add files via upload

---
+++ generateRandomNumber.js
@@ -0,0 +1,18 @@
+// Да се дефинира функцията generateRandomNumber(start, end), която генерира
+// цяло случайно число в интервала, зададен чрез параметрите start и end.
+// Hint: използвайте Math.random() функцията за генериране на случайно число.
+
+// примерно извикване:
+// let randomNumber = generateRandomNumber(1,100);
+// console.log( randomNumber );
+// randomNumber трябва да е цяло число, като 1 >= randomNumber <=100
+
+function generateRandomNumber (arr) {
+    
+    for (i=0; i < 100; i++) {
+console.log(`${i}`);
+    }
+    return (generateRandomNumber)
+}
+let randomNumber = generateRandomNumber(1,100);
+console.log(`Your random number is ${randomNumber}`);
