d5bd929c57fceea257caca094eb13138c06181ce
Mariela Kashukeeva <72321310+mkashukeeva0@users.noreply.github.com>
2021-04-05 17:08:36 +0100

    Add files via upload

---
+++ calculatorTask.js
@@ -0,0 +1,12 @@
+const calculator = {
+    "add": (x,y) => x+y,
+    "sub": (x,y) => x-y,
+    "mult":(x,y) => x*y,
+    "div": (x,y) => x/y,
+    
+}
+console.log( calculator.add(2,3));
+console.log( calculator.sub(2,3));
+console.log( calculator.mult(2,3));
+console.log( calculator.div(2,3));
+
